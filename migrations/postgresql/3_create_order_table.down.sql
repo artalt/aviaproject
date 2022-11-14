0af3b590f9d093c17bd57023cea42b2c493d41b9
araltynbae <araltynbae@mts.ru>
2022-11-14 20:10:31 +0300

    Update migration, update makefile

--- migrations/postgresql/3_create_order_table.down.sql
+++ migrations/postgresql/3_create_order_table.down.sql
@@ -1,4 +1,2 @@
-ALTER TABLE user_order DROP CONSTRAINT FK_user_order_user_id;
-ALTER TABLE user_order DROP CONSTRAINT FK_user_order_order_id;
-DROP TABLE "order";
 DROP TABLE user_order;
+DROP TABLE "order";
