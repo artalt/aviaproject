6fe21041fd616d9efb38a18ab1fd3d0963d10d1e
araltynbae <araltynbae@mts.ru>
2022-11-15 20:14:43 +0300

    Update migrations

--- migrations/postgresql/3_create_order_table.down.sql
+++ migrations/postgresql/3_create_order_table.down.sql
@@ -1,2 +1,1 @@
-DROP TABLE user_order;
 DROP TABLE "order";
--- migrations/postgresql/3_create_order_table.up.sql
+++ migrations/postgresql/3_create_order_table.up.sql
@@ -7,8 +7,3 @@ CREATE TABLE "order"
     updated_at   TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
     PRIMARY KEY (id)
 );
-CREATE TABLE user_order (user_id UUID NOT NULL, order_id UUID NOT NULL, PRIMARY KEY(user_id, order_id));
-CREATE INDEX IDX_user_order_user_id ON user_order (user_id);
-CREATE INDEX IDX_user_order_order_id ON user_order (order_id);
-ALTER TABLE user_order ADD CONSTRAINT FK_user_order_user_id FOREIGN KEY (user_id) REFERENCES "user" (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE;
-ALTER TABLE user_order ADD CONSTRAINT FK_user_order_order_id FOREIGN KEY (order_id) REFERENCES "order" (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE;
